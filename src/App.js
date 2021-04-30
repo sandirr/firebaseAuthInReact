3ccd4ad49c96361b214dd5ca9913b8e2e5325e9a
Andi Irsandi <andi.irsandi765@gmail.com>
2021-04-30 10:39:36 +0800

    login regis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Registrasi from './Components/Registrasi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Route path={["/home", "/"]} exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/registrasi" component={Registrasi} />
+      </Router>
   );
 }
 
