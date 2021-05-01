24b5e96f2bc8e5ee44365573d58088fe242d9399
Andi Irsandi <andi.irsandi765@gmail.com>
2021-05-01 09:28:25 +0800

    forgot-pass

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Registrasi from './Components/Registrasi';
+import ForgotPassword from './Components/ForgotPassword';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path={["/home", "/"]} exact component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/registrasi" component={Registrasi} />
+        <Route path="/forgot-password" component={ForgotPassword} />
       </Router>
   );
 }
--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -3,45 +3,38 @@ import { Button, Container, Grid, TextField } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { firebaseAuthentication } from '../config/firebase'
 
-export default class Login extends Component{
+export default class ForgotPassword extends Component{
     state = {
         email:'',
-        password: ''
     }
     handleChangeField = (e) =>{
         this.setState({[e.target.name]: e.target.value})
     }
     handleSubmit = (e) =>{
         e.preventDefault();
-        const {email, password} = this.state
-        firebaseAuthentication.signInWithEmailAndPassword(email, password)
-        .then(res=>{
-            console.log(res)
-            if(res.user.emailVerified){
-                this.props.history.push('/home')
-            }else{
-                alert('Verifikasi email anda terlebih dahulu!')
-                firebaseAuthentication.signOut()
-            }
+        const {email} = this.state
+        firebaseAuthentication.sendPasswordResetEmail(email)
+        .then(()=>{
+            alert('Silahkan periksa email anda untuk mengubah password')
+            this.props.history.push('/login')
         })
         .catch(error=>{
             alert(error.message)
         })
     }
     render(){
-        const {email, password} = this.state
+        const {email} = this.state
         return(
             <Container>
                 <Grid container justify="center">
                     <Grid xs="12" md="8" lg="4">
-                        <h2>Halaman Login</h2>
+                        <h2>Halaman Forgot Password</h2>
                         <form onSubmit={this.handleSubmit}>
                             <TextField type="email" fullWidth margin="dense" variant="outlined" size="small" value={email} onChange={this.handleChangeField} name="email" label="Email" required />
-                            <TextField type="password" fullWidth margin="dense" variant="outlined" size="small" value={password} onChange={this.handleChangeField} name="password" label="Password" required />
-                            <Button type="submit" fullWidth variant="contained" color="primary">Login</Button>
+                            <Button type="submit" fullWidth variant="contained" color="primary">Kirim Email Reset Password</Button>
                         </form>
                         <p>Belum punya akun? <Link to="/registrasi">Registrasi</Link></p>
-                        <p>Lupa password? <Link to="/forgot-password">Reset Password</Link></p>
+                        <p>Sudah Punya Akun? <Link to="/forgot-password">Login</Link></p>
                     </Grid>
                 </Grid>
             </Container>
